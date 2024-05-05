afe112d320eebb9b96840c2b8d6d1fd003dd7e39
Senal Dilanjana KAD <138982586+5yndr0m@users.noreply.github.com>
2024-05-05 20:07:28 +0530

    added user controllers to perform user actions

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/user.js";
 import { createJWT } from "../utils/index.js";
+import Notice from "../models/notification.js";
 
 export const registerUser = async (req, res) => {
     try {
@@ -75,6 +76,157 @@ export const logoutUser = async (req, res) => {
         });
 
         res.status(200).json({ message: "Logout successfull."});
+    } catch (error) {
+        return res.status(400).json({ status: false, message: error.message });
+    }
+};
+
+export const getTeamList = async (req, res) => {
+    try {
+        const users = await User.find().select("name title role enail isActive");
+
+        res.status(200).json(users);
+    } catch (error) {
+        return res.status(400).json({ status: false, message: error.message });
+    }
+};
+
+export const getNotificationsList = async (req, res) => {
+    try {
+        const { userId } = req.user;
+
+        const notice = await Notice .findOne({
+            team: userId,
+            isRead: { $nin: [userId] },
+        }).populate("task", "title");
+
+        res.status(201).json(notice);
+    } catch (error) {
+        return res.status(400).json({ status: false, message: error.message });
+    }
+};
+
+export const updateUserProfile = async (req, res) => {
+    try {
+        const { userId, isAdmin } = req.user
+        const { _id } = req.body;
+
+        const id = 
+            isAdmin && userId === _id 
+                ? userId 
+                : isAdmin && userId !== _id
+                ? _id
+                : userId;
+
+        const user = await User.findById(id)
+
+        if (user) {
+            user.name = req.body.name || user.name;
+            user.title = req.body.title || user.title;
+            user.role = req.body.role || user.role;
+
+            const updateUser = await User.save();
+
+            user.password = undefined;
+
+            res.status(201).json({
+                status: true,
+                message: "Profile Update Successfully.",
+                user: updateUser,
+            });
+        } else {
+            res.status(404).json({ status: false, message: "User not found" });
+        }
+    } catch (error) {
+        return res.status(400).json({ status: false, message: error.message });
+    }
+};
+
+export const markNotificationRead = async (req, res) => {
+    try {
+        const { userID } = req.user;
+
+        const { isReadType, id } = req.query;
+
+        if (isReadType === "all") {
+            await Notice.updateMany(
+                { team: userId, isRead: { $nin: [userId] }},
+                { $push: {isRead: userId}},
+                { new: true }
+            );
+        } else {
+            await Notice.findOneAndUpdate(
+                { _id: id, isRead: {$nin: [userId] }},
+                { $push: { isRead: userId }},
+                { new: true }
+            );
+        }
+
+        res.status(201).json({ status: true, message: "Done" });
+    } catch (error) {
+        return res.status(400).json({ status: false, message: error.message });
+    }
+};
+
+export const changePassword = async (req, res) => {
+    try {
+        const { userId } = req.user;
+
+        const user = await User.findById(userId);
+
+        if (user) {
+            user.password = req.body.password;
+
+            await user.save();
+
+            user.password = undefined;
+
+            res.status(201).json({
+                status: true,
+                message: `Password changed successfully`,
+            });
+        } else {
+            res.status(404).json({ status: false, message: "User not found" });
+        }
+    } catch (error) {
+        return res.status(400).json({ status: false, message: error.message });
+    }
+};
+
+export const activateUserProfile = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const user = await User.findById(id);
+
+        if (user) {
+            user.isActive = req.body.isActive; //!user.isActive
+
+            await user.save();
+
+            res.status(201).json({
+                status: true,
+                message: `User account has been ${
+                    user?.isActive ? "activated" : "disabled"
+                }`,
+            });
+        } else {
+            res.status(404).json({ status: false, message: "User not found" });
+        }
+    } catch (error) {
+        return res.status(400).json({ status: false, message: error.message });
+    }
+};
+
+export const deleteUserProfile = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        await User.findByIdAndDelete(id);
+
+        res
+            .status(200)
+            .json({ status: true, message: "User deleted successfully" });
     } catch (error) {
         return res.status(400).json({ status: false, message: error.message });
     }
--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,6 +1,17 @@
 import express from 'express';
 import { protectRoute, isAdminRoute } from '../middlewares/authMiddleware.js';
-import { registerUser, loginUser, logoutUser } from '../controllers/userController.js';
+import { 
+    registerUser, 
+    loginUser, 
+    logoutUser, 
+    getTeamList,
+    getNotificationsList,
+    updateUserProfile,
+    markNotificationRead,
+    changePassword,
+    activateUserProfile,
+    deleteUserProfile,
+ } from '../controllers/userController.js';
 
 const router = express.Router();
 
@@ -8,15 +19,18 @@ router.post("/register", registerUser)
 router.post("/login", loginUser)
 router.post("/logout", logoutUser)
 
-// router.get("/get-team", protectRoute, isAdminRoute, getTeamList);
-// router.get("/notifications", protectRoute, getNotificationsList);
+router.get("/get-team", protectRoute, isAdminRoute, getTeamList);
+router.get("/notifications", protectRoute, getNotificationsList);
 
-// router.get("/profile", protectRoute, updateUserProfile);
-// router.get("/read-noti", protectRoute, markNotificationRead);
-// router.get("/change-password", protectRoute, changePassword);
+router.get("/profile", protectRoute, updateUserProfile);
+router.get("/read-noti", protectRoute, markNotificationRead);
+router.get("/change-password", protectRoute, changePassword);
 
-// router
-//     .route("/:id")
-//     .put(protectRoute, isAdminRoute, activateUserProfile);
+// // FOR ADMIN ONLY - ADMIN ROUTES
+
+router
+    .route("/:id")
+    .put(protectRoute, isAdminRoute, activateUserProfile)
+    .delete(protectRoute, isAdminRoute, deleteUserProfile);
     
 export default router;
